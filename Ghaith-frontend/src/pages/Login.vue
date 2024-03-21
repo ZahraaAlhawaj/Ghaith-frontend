58210484f308f9cbccdbb0a6f7002df30be3ce42
Esra <esra.ashoor.e@gmail.com>
2024-03-21 22:14:55 +0300

    worked on the register and log in form

--- Ghaith-frontend/src/pages/Login.vue
+++ Ghaith-frontend/src/pages/Login.vue
@@ -1,11 +1,86 @@
 <script>
-  export default {
-    name: 'Login'
-  }
+export default {
+  name: 'Login'
+}
 </script>
 
 <template>
-  <div>
+  <div class="form-container">
+    <v-sheet class="mx-auto" width="300">
+      <h1 className="account-title">Login</h1>
+      <p className="account-description">Enter your email and password</p>
 
+      <v-form fast-fail @submit.prevent>
+        <v-text-field
+          v-model="email"
+          :rules="emailRules"
+          label="email"
+        ></v-text-field>
+
+        <v-text-field
+          v-model="password"
+          :rules="passwordRules"
+          label="password"
+        ></v-text-field>
+
+        <v-btn rounded="xl" class="mt-2" type="submit" block>Submit</v-btn>
+      </v-form>
+      <div class="register-container">
+        <p class="register">No account?</p>
+        <router-link to="/register" class="register-link">
+          Create account
+        </router-link>
+      </div>
+    </v-sheet>
   </div>
 </template>
+
+<style>
+/* update-form2 */
+
+.account-title {
+  font-size: 24px;
+  text-align: left;
+  color: #10151d;
+}
+
+.account-description {
+  font-size: 16px;
+  text-align: left;
+  color: #555;
+  padding-top: 10px;
+  font-size: 14px;
+  margin-bottom: 20px;
+}
+
+.form-container {
+  width: 30vw;
+  margin: 1em auto;
+  padding: 2em;
+  border-radius: 10px;
+  background-color: #ffffff;
+  border: 2px solid #f2f2f2;
+  margin-top: -0.3%;
+}
+
+.register-link {
+  padding-top: 5%;
+  font-weight: 400px;
+  font-size: 13px;
+}
+
+.register {
+  padding-top: 5%;
+  font-weight: 400px;
+  font-size: 13px;
+  margin-right: 5px;
+}
+
+.register-container {
+  display: flex;
+}
+
+.register-link {
+  color: #4399f6;
+}
+</style>
--- Ghaith-frontend/src/pages/Register.vue
+++ Ghaith-frontend/src/pages/Register.vue
@@ -5,7 +5,69 @@
 </script>
 
 <template>
-  <div>
+  <div class="form-container">
+    <v-sheet class="mx-auto" width="300">
+      <h1 class="account-title">Create Your Account</h1>
+        <p class="account-description">
+          Enter your name, email, and password 
+          to create your account
+        </p>
+      <v-form fast-fail @submit.prevent>
+        <v-text-field
+          v-model="Name"
+          :rules="firstNameRules"
+          label="name"
+        ></v-text-field>
 
+        <v-text-field
+          v-model="email"
+          :rules="emailRules"
+          label="email"
+        ></v-text-field>
+
+        <v-text-field
+          v-model="phoneNumber"
+          :rules="phoneRules"
+          label="phone number"
+        ></v-text-field>
+
+        <v-text-field
+          v-model="birthDate"
+          :rules="birthRules"
+          label="birth date"
+        ></v-text-field>
+
+        <v-btn rounded="xl" class="mt-2" type="submit" block>Submit</v-btn>
+      </v-form>
+    </v-sheet>
   </div>
 </template>
+
+<style>
+/* update-form2 */
+.account-title {
+  font-size: 24px;
+  text-align: left;
+  color: #10151d;
+}
+
+.account-description {
+  font-size: 16px;
+  text-align: left;
+  color: #555;
+  padding-top: 10px;
+  font-size: 14px;
+  margin-bottom: 20px;
+}
+
+.form-container {
+  width: 30vw;
+  margin: 1em auto;
+  padding: 2em;
+  border-radius: 10px;
+  background-color: #ffffff;
+  border: 2px solid #f2f2f2;
+  margin-top: -0.3%;
+}
+</style>
+
