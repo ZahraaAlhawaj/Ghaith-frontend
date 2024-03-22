658f3b26043ec62284065b378a49320d98ec37a4
ZahraaAlhawaj <zahraa.mu2019@gmail.com>
2024-03-22 17:36:00 +0300

    show all cases and all charities in their page

--- Ghaith-frontend/src/pages/Charities.vue
+++ Ghaith-frontend/src/pages/Charities.vue
@@ -1,7 +1,20 @@
 <script>
+import axios from 'axios'
+const API_KEY = import.meta.env.VITE_GHAITH_API
 export default {
   name: 'Charities',
+  data: () => ({
+    charities: null
+  }),
+  mounted() {
+    this.showAllCharity()
+  },
   methods: {
+    async showAllCharity() {
+      const response = await axios.get(`${API_KEY}/charities`)
+      this.charities = response.data
+      console.log(this.charities)
+    },
     showCharity() {
       this.$router.push(`/charity/1`)
     }
@@ -12,13 +25,53 @@ export default {
 <template>
   <h1>All Charities</h1>
   <v-card
+    v-for="charity in charities"
+    :key="charity._id"
     class="mx-auto"
     height="200"
-    image="https://cdn.vuetifyjs.com/images/cards/docks.jpg"
+    :image="charity.logo"
     max-width="300"
     max-height="350"
     theme="dark"
-    title="Card title"
     @click="showCharity"
-  ></v-card>
+  >
+    <v-hover>
+      <template v-slot:default="{ hover }">
+        <div
+          class="card-image"
+          :style="{ 'background-image': `url(${charity.logo})` }"
+        >
+          <div v-if="hover" class="title-overlay">
+            {{ charity.title }}
+          </div>
+        </div>
+      </template>
+    </v-hover>
+  </v-card>
 </template>
+
+<style>
+.v-card-title {
+  color: red !important;
+}
+
+.card-image {
+  position: relative;
+  width: 100%;
+  height: 100%;
+  background-size: cover;
+  background-position: center;
+}
+
+.title-overlay {
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  width: 100%;
+  padding: 10px;
+  background-color: rgba(0, 0, 0, 0.5);
+  color: white;
+  text-align: center;
+  font-size: 18px;
+}
+</style>
