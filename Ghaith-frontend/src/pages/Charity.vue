4433b93a6add06373296f9a01c6744c4fde29fde
ZahraaAlhawaj <zahraa.mu2019@gmail.com>
2024-03-22 21:23:59 +0300

    display charity details

--- Ghaith-frontend/src/pages/Charity.vue
+++ Ghaith-frontend/src/pages/Charity.vue
@@ -1,9 +1,33 @@
 <script>
+import axios from 'axios'
+const API_KEY = import.meta.env.VITE_GHAITH_API
 export default {
-  name: 'Charity'
+  name: 'Charity',
+  data: () => ({
+    charity: [],
+    charityId: 0
+  }),
+  mounted() {
+    this.charityId = this.$route.params.id
+    this.showCharity(this.charityId)
+  },
+  methods: {
+    async showCharity(charityId) {
+      const response = await axios.get(`${API_KEY}/charities/${charityId}`)
+      this.charity = response.data
+    }
+  }
 }
 </script>
 <template>
-  <h1>Charity Page</h1>
+  <h1>{{ charity.name }}</h1>
+  <img
+    height="200"
+    max-width="200"
+    max-height="200"
+    :src="charity.logo"
+    alt=""
+  />
+  <p v-if="charity.user">{{ charity.user.email }}</p>
 </template>
-<style></style>
+<style scoped></style>
