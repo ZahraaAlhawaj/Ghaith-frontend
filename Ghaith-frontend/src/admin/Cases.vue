f4de94ceb5b0a0c48123edce0491acd0668b16a1
zahraa <zahraaalihasan279@gmail.com>
2024-03-26 21:31:20 +0300

    update case / delete

--- Ghaith-frontend/src/admin/Cases.vue
+++ Ghaith-frontend/src/admin/Cases.vue
@@ -6,6 +6,12 @@ import { createCase, updateCase, deleteCase } from '../services/case'
 import { getCategories } from '../services/category'
 export default {
   name: 'Cases',
+  setup() {
+    const store = useStore()
+    const user = computed(() => store.getters.currentUser)
+
+    return { user }
+  },
   data: function () {
     return {
       search: '',
@@ -16,22 +22,29 @@ export default {
         { text: 'Actions', value: 'actions', sortable: false }
       ],
       createDialog: false,
+      updateDialog: {},
       formValues: {
         name: '',
         image: '',
         description: '',
         total_amount: null,
         start_date: new Date(),
-        end_date: new Date()
+        end_date: new Date(),
+        category: null
       }
     }
   },
   mounted() {
-    this.getAllCategories()
+    this.getAllCategories(), this.getAllCases()
   },
   methods: {
+    async getAllCases() {
+      this.cases = await showCharityCases(this.user.charityId)
+      // console.log(this.cases)
+    },
     async getAllCategories() {
       this.categories = await getCategories()
+      // console.log(this.categories)
     },
     async handleSubmit(event, action, item = null) {
       event.preventDefault()
@@ -66,6 +79,26 @@ export default {
         }
       }
       return false
+    },
+    async deleteOneCase(selectedCase) {
+      console.log('delete function')
+      const deletedCase = await deleteCase(selectedCase._id)
+      this.getAllCases()
+    },
+    openUpdateDialog(item) {
+      this.formValues = {
+        name: item.name,
+        image: item.image,
+        description: item.description,
+        total_amount: item.total_amount,
+        end_date: item.end_date,
+        start_date: item.start_date,
+        category: item.category
+      }
+      this.updateDialog = {
+        ...this.updateCase,
+        [item._id]: true
+      }
     }
   }
 }
@@ -92,6 +125,7 @@ export default {
               item-text="name"
               label="Category"
             ></v-select>
+
             <v-text-field
               v-model="formValues['name']"
               label="Name"
@@ -142,6 +176,115 @@ export default {
         </template>
       </v-card>
     </v-dialog>
+  </v-container>
+  <v-container>
+    <v-card flat>
+      <v-card-title class="d-flex align-center pe-2">
+        <v-text-field
+          v-model="search"
+          density="compact"
+          label="Search"
+          prepend-inner-icon="mdi-magnify"
+          variant="solo-filled"
+          flat
+          hide-details
+          single-line
+        ></v-text-field>
+      </v-card-title>
+      <v-data-table :headers="headers" :items="cases" :search="search">
+        <template v-slot:item.actions="{ item }">
+          <v-dialog v-model="updateDialog[item._id]" max-width="400" persistent>
+            <template v-slot:activator="{ on, attrs }">
+              <v-btn
+                @click="openUpdateDialog(item)"
+                v-bind="attrs"
+                class="mr-4"
+                color="primary"
+              >
+                Update
+              </v-btn>
+            </template>
+
+            <v-card prepend-icon="mdi-map-marker">
+              <v-sheet class="list-form">
+                <h3>Update Case</h3>
+                <v-form fast-fail @submit.prevent @submit="handleSubmit">
+                  <v-select
+                    v-model="formValues['category']"
+                    :items="categories"
+                    item-value="_id"
+                    item-text="name"
+                    label="Category"
+                  ></v-select>
+                  <v-text-field
+                    v-model="formValues['name']"
+                    label="Name"
+                  ></v-text-field>
+                  <v-textarea
+                    v-model="formValues['description']"
+                    label="Description"
+                  ></v-textarea>
+
+                  <v-text-field
+                    v-model="formValues['total_amount']"
+                    label="Total Amount"
+                    type="number"
+                  ></v-text-field>
+                  <v-text-field
+                    v-model="formValues['start_date']"
+                    label="Start Date"
+                    type="date"
+                  ></v-text-field>
+                  <v-text-field
+                    v-model="formValues['end_date']"
+                    label="End Date"
+                    type="date"
+                  ></v-text-field>
+                  <v-text-field
+                    v-model="formValues['image']"
+                    label="Image"
+                  ></v-text-field>
+                </v-form>
+              </v-sheet>
+
+              <template v-slot:actions>
+                <v-spacer></v-spacer>
+
+                <v-btn @click="updateDialog[item._id] = false"> Dismiss </v-btn>
+
+                <v-btn
+                  type="submit"
+                  @click="
+                    (event) => {
+                      if (handleSubmit(event, 'update', item)) {
+                        updateDialog = false
+                      }
+                    }
+                  "
+                >
+                  Update
+                </v-btn>
+              </template>
+            </v-card>
+          </v-dialog>
+
+          <v-btn @click="deleteOneCase(item)" color="primary" text>
+            Delete
+          </v-btn>
+        </template>
+        <template v-slot:headers="props">
+          <tr>
+            <th>Name</th>
+            <th>Action</th>
+          </tr>
+        </template>
+        <template v-slot:items="props">
+          <tr>
+            <td>{{ item.name }}</td>
+          </tr>
+        </template>
+      </v-data-table>
+    </v-card>
   </v-container>
 </template>
 
