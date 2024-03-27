eb01049ee8dc47e6fb0f1ba5dbebb38a16e6f61c
Esra <esra.ashoor.e@gmail.com>
2024-03-27 14:03:57 +0300

    worked on evevnt page

--- Ghaith-frontend/src/pages/Events.vue
+++ Ghaith-frontend/src/pages/Events.vue
@@ -49,36 +49,51 @@ export default {
 
 <template>
   <div>
-    <v-alert v-if="alert.show" :type="alert.type" density="compact">
+    <v-alert
+      v-if="alert.show"
+      :type="alert.type"
+      density="compact"
+      class="alert-color"
+    >
       {{ alert.message }}
     </v-alert>
     <v-row>
       <v-col cols="4" v-for="event in events" :key="event._id">
-        <v-card>
+        <v-card class="card-outline mb-4">
           <v-img
             :src="event.image"
             height="200"
             :aspect-ratio="16 / 9"
             gradient="to bottom, rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.7)"
           ></v-img>
-          <div class="pa-2">
-            <v-card-title class="mb-2">
-              <v-icon left>mdi-map-marker</v-icon>
-              {{ event.location }}
-            </v-card-title>
-            <v-card-text class="mb-2">
+          <div class="mb-2 card-info">
+            <v-icon left>mdi-map-marker</v-icon>
+            {{ event.location }}
+          </div>
+          <div
+            class="pa-2 card-container"
+            style="text-align: left; color: #4b5f23"
+          >
+            <div>
               <v-icon left>mdi-clock</v-icon>
               {{ event.time }}
-            </v-card-text>
+            </div>
+
             <v-card-text>{{ event.description }}</v-card-text>
-            <v-card-text>
+
+            <div>
               <v-icon left>mdi-account-group</v-icon>
               {{ event.volunteers.length }} /
               {{ event.requiredVolunteers }} joined
-            </v-card-text>
+            </div>
           </div>
+
           <v-card-actions>
-            <v-btn text :disabled="!user" @click.stop="joinEvent(event._id)"
+            <v-btn
+              class="status-btn"
+              text
+              :disabled="!user"
+              @click.stop="joinEvent(event._id)"
               >Join Event</v-btn
             >
           </v-card-actions>
@@ -87,3 +102,44 @@ export default {
     </v-row>
   </div>
 </template>
+
+<style scoped>
+.card2 {
+  height: 5%;
+}
+.card-outline {
+  margin-left: 20%;
+  margin-right: 20%;
+  margin-top: 5%;
+}
+
+.card-info {
+  background-color: #4b5f23;
+  color: #e6e5d0;
+  padding: 5%;
+}
+
+.card-container {
+  text-align: left;
+}
+
+.card-outline {
+  border: 0.12em solid #4b5f23;
+  border-radius: 0.5em;
+  background-color: #e6e5d0;
+  width: 80%;
+  height: 100%;
+}
+
+.bg-warning {
+  margin-top: 0% !important;
+  background-color: #96a640 !important;
+  color: #4b5f23 !important;
+}
+
+.status-btn {
+  margin-top: 0%;
+  color: #4b5f23;
+  background-color: #b1bf5c;
+}
+</style>
